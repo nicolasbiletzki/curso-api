000df5a140e3247789d8d0ac65cdc44581ecd327
Nicolas Biletzki <nicolasbiletzki@gmail.com>
2022-02-01 12:18:18 -0300

    Iniciando Projeto

---
+++ curso-api/Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using curso_api.Models.Usuarios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace curso_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+    }
+}
