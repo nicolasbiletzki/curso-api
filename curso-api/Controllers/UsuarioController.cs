d7b0dca9025db07c9d86d780244b11b98b56dbb2
Nicolas Biletzki <nicolasbiletzki@gmail.com>
2022-02-01 15:05:34 -0300

    Testando POST via Postman

--- curso-api/Controllers/UsuarioController.cs
+++ curso-api/Controllers/UsuarioController.cs
@@ -3,12 +3,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace curso_api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/usuario")]
     [ApiController]
     public class UsuarioController : ControllerBase
     {
         [HttpPost]
+        [Route("logar")]
         public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Ok(loginViewModelInput);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        public IActionResult Registrar(RegistroViewModelInput loginViewModelInput)
         {
             return Created("", loginViewModelInput);
         }
